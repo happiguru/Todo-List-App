e08b560e9f6055e2caa5571c2557ed03ec1878b4
hguruman <hguruman@gmail.com>
2021-02-19 20:13:36 +0400

    Add dev mode to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 // Webpack uses this to work with directories
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
-
+  context: __dirname,
   // Path to your entry point. From this file Webpack will begin his work
   entry: './src/javascript/index.js',
 
@@ -14,10 +15,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  plugins: [
+    new HtmlWebPackPlugin()
+  ]
 
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript
   // minifying and other thing so let's set mode to development
-  mode: 'development',
 };
